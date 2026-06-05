4d247d8c79dcaec6171aabaf8478f4b7e3c0f75a
mistvle <mistle.roblox@gmail.com>
2026-06-05 00:32:14 -0700

    A

--- utils/purchaseLogger.js
+++ utils/purchaseLogger.js
@@ -15,7 +15,7 @@ module.exports = async (client) => {
 
             console.log("🔍 Checking purchases...");
 
-            const channel = client.channels.cache.get(CHANNEL_ID);
+            const channel = client.channels.cache.get("1510892474971852810");
             if (!channel) return;
 
             const sales = await noblox.getGroupTransactions(
@@ -25,82 +25,80 @@ module.exports = async (client) => {
     25
 );
 
-console.log("TRANSACTION:");
-console.log(JSON.stringify(sales[0], null, 2));
-return;
 
             for (const sale of sales.reverse()) {
 
-                const exists = client.db.prepare(`
-                    SELECT *
-                    FROM purchase_logs
-                    WHERE transaction_id = ?
-                `).get(String(sale.id));
+    const transactionId = sale.purchaseToken;
 
-                if (exists) continue;
+    const exists = client.db.prepare(`
+        SELECT *
+        FROM purchase_logs
+        WHERE transaction_id = ?
+    `).get(transactionId);
 
-                client.db.prepare(`
-                    INSERT INTO purchase_logs (
-                        transaction_id
-                    )
-                    VALUES (?)
-                `).run(String(sale.id));
+    if (exists) continue;
 
-                const amountReceived =
-                    Math.floor(sale.price * 0.7);
+    client.db.prepare(`
+        INSERT INTO purchase_logs (
+            transaction_id
+        )
+        VALUES (?)
+    `).run(transactionId);
 
-                const userLink =
-                    `https://www.roblox.com/users/${sale.creator.id}/profile`;
+    const amountReceived = sale.currency.amount;
 
-                const gamepassLink =
-                    `https://www.roblox.com/catalog/${GAMEPASS_ID}/Payment`;
+    const userLink =
+        `https://www.roblox.com/users/${sale.agent.id}/profile`;
 
-                await channel.send({
-                    flags: 32768,
-                    components: [
-                        {
-                            type: 17,
-                            components: [
-                                {
-                                    type: 10,
-                                    content: "# <:wallet:1510911931735740457> Purchase Log"
-                                },
-                                {
-                                    type: 14,
-                                    spacing: 2
-                                },
-                                {
-                                    type: 10,
-                                    content:
+    const gamepassLink =
+        `https://www.roblox.com/catalog/${sale.details.id}/${encodeURIComponent(sale.details.name)}`;
+
+    await channel.send({
+        flags: 32768,
+        components: [
+            {
+                type: 17,
+                components: [
+                    {
+                        type: 10,
+                        content: "# <:wallet:1510911931735740457> Purchase Log"
+                    },
+                    {
+                        type: 14,
+                        spacing: 2
+                    },
+                    {
+                        type: 10,
+                        content:
 `A new purchase has been made. View information regarding it below.
 
-**Username:** [${sale.creator.name}](${userLink})
-**Gamepass Name:** [Payment](${gamepassLink})
+**Username:** [${sale.agent.name}](${userLink})
+**Gamepass Name:** [${sale.details.name}](${gamepassLink})
 **Amount Received:** ${amountReceived}`
-                                },
-                                {
-                                    type: 14,
-                                    spacing: 2
-                                },
-                                {
-                                    type: 12,
-                                    items: [
-                                        {
-                                            media: {
-                                                url: "https://media.discordapp.net/attachments/1510896301028409465/1510896476837122180/footer_v2.png?ex=6a23c17f&is=6a226fff&hm=b3f6fba7649114e335c42eb44dba97f5a5c3a5b7f5f89163c02810e6c90cad79&=&format=webp&quality=lossless"
-                                            }
-                                        }
-                                    ]
+                    },
+                    {
+                        type: 14,
+                        spacing: 2
+                    },
+                    {
+                        type: 12,
+                        items: [
+                            {
+                                media: {
+                                    url: "https://media.discordapp.net/attachments/1510896301028409465/1510896476837122180/footer_v2.png?ex=6a23c17f&is=6a226fff&hm=b3f6fba7649114e335c42eb44dba97f5a5c3a5b7f5f89163c02810e6c90cad79&=&format=webp&quality=lossless"
                                 }
-                            ]
-                        }
-                    ]
-                });
-
-                console.log(
-                    `💰 Logged purchase from ${sale.creator.name}`
-                );
+                            }
+                        ]
+                    }
+                ]
             }
+        ]
+    });
+
+    console.log(
+        `💰 Logged purchase from ${sale.agent.name}`
+    );
+}
 
         } catch (err) {
             console.error(
